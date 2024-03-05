693fe1341ba080dddc07cc3b6d0f9730fbcc0b1c
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2024-03-05 01:19:19 +0100

    add-lesson25

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Lesson21 from "lessons/Lesson21/Lesson21";
 import Lesson22 from "lessons/Lessons22/Lesson22";
 import Lesson23 from "lessons/Lesson23/Lesson23";
 import Lesson24 from "lessons/Lesson24/Lesson24";
+import Lesson25 from "lessons/Lesson25/Lesson25";
 
 // Homeworks
 import Homework20 from "homeworks/Homework20/Homework20";
 import Homework21 from "homeworks/Homework21/Homework21";
 import Homework22 from "homeworks/Homework22/Homework22";
 import Homework23 from "homeworks/Homework23/Homework23";
+import Homework24 from "homeworks/Homework24/Homework24";
 
 function App() {
   return (
@@ -21,11 +23,13 @@ function App() {
       {/* <Lesson21 /> */}
       {/* <Lesson22 /> */}
       {/* <Lesson23 /> */}
-      <Lesson24 />
+      {/* <Lesson24 /> */}
+      <Lesson25 />
       {/* <Homework20 /> */}
       {/* <Homework21 /> */}
       {/* <Homework22 /> */}
       {/* <Homework23 /> */}
+      {/* <Homework24 /> */}
     </>
   );
 }
